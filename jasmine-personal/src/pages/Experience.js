0651d8f3f689c18d84a4913b40edb08c7c814b4a
jschab02SH <jschab02@volunteers.seeinghands.org>
2025-06-06 23:12:07 -0400

    button list not working

--- jasmine-personal/src/pages/Experience.js
+++ jasmine-personal/src/pages/Experience.js
@@ -3,10 +3,14 @@ import './Experience.css';
 function Experience() {
   return (
     <body> 
-      <div className="header"> Projects/Experience </div>    
-      <div className="left-list"> 
-        
-      </div>
+      <div className="header"> Projects/Experience
+        <div className="left-list"> 
+          <button class="button">AI Outfit Curator</button>
+          <button class="button">Personal Blog</button>
+          <button class="button">A2Empowerment</button>
+          <button class="button">RateMyDiningHall</button>
+        </div>
+      </div>    
     </body>
   );
 }
