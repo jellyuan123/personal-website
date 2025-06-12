137768981b62ffea2c8f948b9484c08b0137056c
jschab02SH <jschab02@volunteers.seeinghands.org>
2025-06-11 22:46:16 -0400

    trial experience but projects is unaligned

--- jasmine-personal/src/pages/Experience.js
+++ jasmine-personal/src/pages/Experience.js
@@ -1,17 +1,42 @@
 import './Experience.css';
+import { useState } from 'react';
+import * as React from "react";
+import JobBox from '../components/JobBox';
 
 function Experience() {
+  const [selectedJob, setSelectedJob] = useState("AI Outfit Curator");
+
+  const handleClick = (event) => {
+    event.target.scrollIntoView({ behavior: 'smooth' });
+    const currentJob = event.target.textContent;
+    setSelectedJob(currentJob);
+  };
+
   return (
-    <body> 
-      <div className="header"> Projects/Experience
-        <div className="left-list"> 
-          <button class="button">AI Outfit Curator</button>
-          <button class="button">Personal Blog</button>
-          <button class="button">A2Empowerment</button>
-          <button class="button">RateMyDiningHall</button>
+    <div> 
+      <div className="header">Projects/Experience<div className="left-list">
+        <button className="big-button">Projects</button>
+          <div className="dropdown-list">
+            <button className="button" onClick={handleClick}>AI Outfit Curator</button>
+            <button className="button" onClick={handleClick}>Personal Blog</button>
+            <button className="button" onClick={handleClick}>RateMy DiningHall</button>
+          </div>
+
+          <div className="dropdown">
+            <button className="big-button">Experience</button>
+              <div className="dropdown-list">
+                <button className="button" onClick={handleClick}>Groundwork Somerville</button>
+                <button className="button" onClick={handleClick}>Seeing Hands</button>
+                <button className="button" onClick={handleClick}>A2 Empowerment</button>
+              </div>
+          </div>
+
         </div>
-      </div>    
-    </body>
+        <div id="job-display">
+          {selectedJob && <JobBox currentJob={selectedJob} />}
+        </div>
+      </div>
+    </div>
   );
 }
 
